4b7f6129c1d75550a2d0a605bd0b67667950e83a
JRudransh <JRudransh@protonmail.com>
2022-01-10 18:53:02 +0530

    JS Operators

---
+++ main.js
@@ -0,0 +1,86 @@
+// Variables
+// var x = 5.2
+// let y = 'Jay'
+
+// Constant
+// const p = 'Hello'
+
+// {
+//     let z = 5
+//     console.log(z);
+// }
+// console.log(z);
+
+
+// Datatype
+// Numbers
+// int
+// 1,2,3,4,98, ---
+
+// float
+// 1.2, 4.5, 7.5
+
+// hex
+// 0xcc
+
+// String
+// 'Hello'
+// "Hello"
+
+// Template litrels
+// let greet = `Hello ${y}`
+
+// Boolean
+// true
+// false
+
+// undefined
+// let z;
+
+// null
+// let n = null;
+
+
+
+// Arithnatic
+// +
+// -
+// /
+// %
+// *
+// **
+// ++
+// --
+
+
+// Assignment
+// =    | x = 10
+// +=   | x += 2    |   x = x + 2
+// -=   | x -= 2    |   x = x - 2
+// *=   | x *= 2    |   x = x * 2
+// /=   | x /= 2    |   x = x / 2
+// %=   | x %= 3    |   x = x % 2
+// **=  | x **= 2   |   x = x ** 2
+
+
+// Comparision
+// ==       equal to
+// ===      equal value and equal type
+// !=       not equal to
+// !==      not equal value or not equal type
+// >        grater than
+// <        less than
+// >=       grater than or equal to
+// <=       less than or equal to
+
+
+// Logical
+// &&   logical and
+// ||   logical or
+// !    logical not
+
+
+// Type
+// typeof
+// instanceof
+
